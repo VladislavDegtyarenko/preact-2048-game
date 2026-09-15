a45201ba13ef27df0d82ca01a3ecdf5244f35c78
Vladyslav Dihtiarenko <vladislavdegtyarenko@gmail.com>
2026-09-15 21:56:39 +0200

    fast animations query param

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,11 @@
-export const ANIMATION_DURATION = 200; // in ms
+const DEFAULT_ANIMATION_DURATION = 200;
+const FAST_ANIMATION_DURATION = DEFAULT_ANIMATION_DURATION / 1.5;
+
+const FAST_ANIMATIONS_SEARCH_PARAM = "fastAnimations";
+const isFastAnimations = new URLSearchParams(window.location.search).has(
+  FAST_ANIMATIONS_SEARCH_PARAM,
+);
+
+export const ANIMATION_DURATION = isFastAnimations
+  ? FAST_ANIMATION_DURATION
+  : DEFAULT_ANIMATION_DURATION; // in ms
